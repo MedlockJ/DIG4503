53bf5d98da5ef1fbfe1b1fc5778170967e4bc9dc
MedlockJ <70552237+MedlockJ@users.noreply.github.com>
2021-03-29 11:15:50 -0400

    lab10 init

---
+++ lab10/index.js
@@ -0,0 +1,59 @@
+import Express from "express";
+import fs from "fs";
+import cors from "cors";
+
+const App = Express();
+const port = 3010;
+
+App.use(cors());
+
+let fileContents = fs.readFileSync("database.json");
+
+let database = JSON.parse(fileContents);
+
+App.use("/", Express.static("public"));
+
+App.get("/employees/:name", (req,res)=>{
+    let result = {"error": "Not available"};
+
+    database.forEach((value)=>{
+        if(req.params.name == value.name){
+            result = value;
+        }
+
+    });
+
+    res.json(result);
+});
+
+App.get("/ages/:number", (req, res) => {
+    let result = {"error": "Not available"};
+
+    database.forEach((value)=>{
+        if(req.params.number == value.age){
+            result = value;
+        }
+
+    });
+
+    res.json(result);
+
+});
+
+App.post("/employees/:name/:age", (req, res) => {
+    let result = {
+        "name": req.params.name,
+        "age": parseInt(req.params.age)
+    };
+
+    database.push(result);
+
+    fs.writeFileSync("database.json", JSON.stringify(database, null, '\t'));
+
+    res.json(result);
+})
+
+App.listen(port, ()=>{
+    console.log("Server Running!");
+
+});
